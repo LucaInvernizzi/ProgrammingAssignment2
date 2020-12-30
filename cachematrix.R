3bb401f8db4149a0a81aee7fe13784f9a0e9ba25
LucaInvernizzi <hofinitoleopzionitk@gmail.com>
2020-12-30 16:27:42 +0100

    Solved exercise

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { #x is initiate here as an object
+        inv <- NULL # intiate inv
+        set <- function(y) {
+                x <<- y # function which assign value to x
+                inv <<- NULL # same function "clean" inv is there are leftovers
+        }
+        get <- function() x # function to access x
+        setinv <- function(solve) inv <<- solve # takes value from cacheSolve
+        getinv <- function() inv # function to access inv
+        list(set = set, get = get, # this saves functions as objects to be used
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv() # tries to get the inv matrix if already there
+        if(!is.null(inv)) { # if not NULL retrieve the stored matrix
+                message("getting cached data")
+                return(inv) 
+        }
+        data <- x$get() # if not there takes the vector
+        inv <- solve(data, ...) # use the vector to calculate inv matrix
+        x$setinv(inv) # set inv matrix in object create from makeCacheMatrix
+        inv # return inv matrix
 }
